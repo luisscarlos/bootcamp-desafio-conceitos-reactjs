50df0fe8eb98ce41dfe825d82887778d63732097
Luis Carlos Santos Filho <500500189@geq.com.br>
2020-04-12 06:10:52 -0300

    challenge finished

--- src/App.js
+++ src/App.js
@@ -1,12 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import api from './services/api';
 
 import "./styles.css";
 
-import Repository from './components/Repository';
-
 function App() {
+  const [repository, setRepository] = useState([]);
 
-  return (<Repository />);
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepository(response.data);
+    });
+  }, []);
+
+  async function handleAddRepository() {
+        const response = await api.post('repositories', {
+          title: 'Be The Hero',
+          url: 'https://github.com/luisscarlos/be-the-hero',
+          techs: ['Node.js', 'ReactJS'],
+        });
+
+        setRepository([...repository, response.data]);
+  }
+
+  async function handleRemoveRepository(id) {
+      await api.delete(`repositories/${id}`);
+
+      const newRepository = repository.filter(repo => repo.id !== id)
+      setRepository(newRepository);
+  }
+
+  return (
+    <div>
+      <ul data-testid="repository-list">
+        {repository.map(repo => (
+            <li key={repo.id}>
+              {repo.title}
+
+              <button onClick={() => handleRemoveRepository(repo.id)}>
+                Remover
+              </button>
+            </li>   
+          ))}
+      </ul>
+      
+      <button onClick={handleAddRepository}>Adicionar</button>
+    </div>
+  );
 }
 
 export default App;
